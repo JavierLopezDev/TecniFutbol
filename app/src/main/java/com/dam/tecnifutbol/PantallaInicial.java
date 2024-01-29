926120e93ecfcac532a49ffcffa7c6c14de038a4
S4nch3z04 <alvaro.schz.01@gmail.com>
2024-01-29 12:41:13 +0100

    Agregada la pantalla inicial de perfiles con los diseños.

---
+++ app/src/main/java/com/dam/tecnifutbol/PantallaInicial.java
@@ -0,0 +1,39 @@
+package com.dam.tecnifutbol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridLayout;
+import android.widget.TextView;
+
+import com.dam.tecnifutbol.R;
+
+public class PantallaInicial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.p_inicial);
+        TextView etEntrenador = findViewById(R.id.et_entrenador);
+        TextView etJugador = findViewById(R.id.et_jugador);
+
+        //Configuración botones
+        etEntrenador.setTextSize(30);
+        etJugador.setTextSize(30);
+        etEntrenador.setTextColor(getResources().getColor(R.color.white));
+        etJugador.setTextColor(getResources().getColor(R.color.white));
+        etEntrenador.setBackgroundResource(R.drawable.boton);
+        etJugador.setBackgroundResource(R.drawable.boton);
+
+        etEntrenador.setOnClickListener(v -> {
+            //Nos movemos a la pantalla de login
+            setContentView(R.layout.login_firebase);
+
+        });
+
+
+
+    }
+}
