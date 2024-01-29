b617d6064ff19194ac9054c1656589fa6e8f5f8b
Izan30 <izangallegotorres@gmail.com>
2024-01-29 13:00:16 +0100

    Pantalla de los entrenamientos (RecyclerView, CardView) sin datos

---
+++ app/src/main/java/com/dam/tecnifutbol/AdaptadorEntrenamientos.java
@@ -0,0 +1,30 @@
+package com.dam.tecnifutbol;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class AdaptadorEntrenamientos extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview_entrenamientos, parent, false);
+
+        return new RecyclerView.ViewHolder(view) {
+        };
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
---
+++ app/src/main/java/com/dam/tecnifutbol/PantallaEntrenamientos.java
@@ -0,0 +1,14 @@
+package com.dam.tecnifutbol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PantallaEntrenamientos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.p_entrenamientos);
+    }
+}
