649f455cb6b5dda77ea49ebe268dc53364f6e50e
Izan30 <izangallegotorres@gmail.com>
2024-01-29 13:26:34 +0100

    Base de Datos creada (TecniFutbol) junto con la tabla "entrenamientos"

--- app/src/main/java/com/dam/tecnifutbol/MainActivity.java
+++ app/src/main/java/com/dam/tecnifutbol/MainActivity.java
@@ -2,14 +2,20 @@ package com.dam.tecnifutbol;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private SQLiteDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        database = openOrCreateDatabase("TecniFutbol", MODE_PRIVATE, null);
+        database.execSQL("CREATE TABLE IF NOT EXISTS entrenamientos " +
+                "(id INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT, categoria TEXT, duracion TEXT, " +
+                "descripcion TEXT, repeticiones INTEGER, material TEXT)");
     }
 }
