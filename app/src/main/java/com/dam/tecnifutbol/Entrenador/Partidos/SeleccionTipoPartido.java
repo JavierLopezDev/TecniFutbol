c7796bf51b86f75cd58c611701cb8d846b1da016
JavierLopezDev <javierlopezandujar2003@gmail.com>
2024-01-30 17:15:31 +0100

    Configurar el camino de la app hasta la seleccion del tipo de partido.

---
+++ app/src/main/java/com/dam/tecnifutbol/Entrenador/Partidos/SeleccionTipoPartido.java
@@ -0,0 +1,16 @@
+package com.dam.tecnifutbol.Entrenador.Partidos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dam.tecnifutbol.R;
+
+public class SeleccionTipoPartido extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seleccion_tipo_partido);
+    }
+}
