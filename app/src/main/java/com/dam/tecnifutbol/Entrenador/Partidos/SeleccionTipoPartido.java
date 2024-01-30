5f1379a8f3ddc8178c569d366a85bc9730126e9b
JavierLopezDev <javierlopezandujar2003@gmail.com>
2024-01-30 17:50:33 +0100

    Agregar la pantalla de la seleccion del tipo de partido con la funcionalidad y el diseño (sujeto a cambios)

--- app/src/main/java/com/dam/tecnifutbol/Entrenador/Partidos/SeleccionTipoPartido.java
+++ app/src/main/java/com/dam/tecnifutbol/Entrenador/Partidos/SeleccionTipoPartido.java
@@ -2,15 +2,39 @@ package com.dam.tecnifutbol.Entrenador.Partidos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.dam.tecnifutbol.R;
 
 public class SeleccionTipoPartido extends AppCompatActivity {
 
+    TextView tv_pEntrenamiento, tv_pVsEquipo, tv_back;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seleccion_tipo_partido);
+
+        tv_pEntrenamiento = findViewById(R.id.tv_pEntrenamiento);
+        tv_pVsEquipo = findViewById(R.id.tv_pVsEquipo);
+        tv_back = findViewById(R.id.tv_back);
+
+        tv_pEntrenamiento.setOnClickListener(view -> {
+            /*Intent intent = new Intent(SeleccionTipoPartido.this, {NombreClase}.class);
+            startActivity(intent);*/
+        });
+
+        tv_pVsEquipo.setOnClickListener(view -> {
+            /*Intent intent = new Intent(SeleccionTipoPartido.this, {NombreClase}.class);
+            startActivity(intent);*/
+        });
+
+        tv_back.setOnClickListener(view -> {
+            Intent intent = new Intent(SeleccionTipoPartido.this, SeleccionDeEquipos.class);
+            startActivity(intent);
+        });
+
     }
 }
