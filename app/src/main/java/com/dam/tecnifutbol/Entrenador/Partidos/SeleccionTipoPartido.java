a5f3b965ac0c32f6ee23771bb5e903082bce8c87
JavierLopezDev <javierlopezandujar2003@gmail.com>
2024-02-05 13:33:44 +0100

    Modificar el diseño de los botones en la pantalla de SeleccionTipoPartido

--- app/src/main/java/com/dam/tecnifutbol/Entrenador/Partidos/SeleccionTipoPartido.java
+++ app/src/main/java/com/dam/tecnifutbol/Entrenador/Partidos/SeleccionTipoPartido.java
@@ -2,30 +2,29 @@ package com.dam.tecnifutbol.Entrenador.Partidos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.LinearLayout;
 
 import com.dam.tecnifutbol.R;
 
 public class SeleccionTipoPartido extends AppCompatActivity {
 
-    TextView tv_pEntrenamiento, tv_pVsEquipo;
+    LinearLayout ly_pEntrenamiento, ly_pVsEquipo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seleccion_tipo_partido);
 
-        tv_pEntrenamiento = findViewById(R.id.tv_pEntrenamiento);
-        tv_pVsEquipo = findViewById(R.id.tv_pVsEquipo);
+        ly_pEntrenamiento = findViewById(R.id.ly_pEntrenamiento);
+        ly_pVsEquipo = findViewById(R.id.ly_pVsEquipo);
 
-        tv_pEntrenamiento.setOnClickListener(view -> {
+        ly_pEntrenamiento.setOnClickListener(view -> {
             /*Intent intent = new Intent(SeleccionTipoPartido.this, {NombreClase}.class);
             startActivity(intent);*/
         });
 
-        tv_pVsEquipo.setOnClickListener(view -> {
+        ly_pVsEquipo.setOnClickListener(view -> {
             /*Intent intent = new Intent(SeleccionTipoPartido.this, {NombreClase}.class);
             startActivity(intent);*/
         });
